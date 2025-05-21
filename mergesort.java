47de3e7e7e60ecb6b198ba3a40ac4cea139ef73c
swatibhatt123 <swatibhatt678@gmail.com>
2025-05-21 22:01:50 +0530

    mergesort  algorithm

--- mergesort.java
+++ mergesort.java
@@ -20,13 +20,41 @@ public class mergesort {
         divideArray(0,length-1);
     }
 
-    public void divideArray(int lowerindex ,int higherindex){
+    public void divideArray(int lowerindex ,int higherindex){ ///using recursion
         if(lowerindex < higherindex){
             int middle=lowerindex+(higherindex -lowerindex)/2;
             divideArray(lowerindex, middle); //sort left side
             divideArray(middle + 1, higherindex); //sort right side
-        
+            mergeArray(lowerindex ,middle ,higherindex);
+        }
+    }
 
+    public void mergeArray(int lowerindex , int middle ,int higherindex){
+        for(int i=lowerindex ;i<= higherindex ; i++){
+            tempMergeArr[i]=array[i];
+        }
+
+        int i=lowerindex;
+        int j=middle+1;
+        int k=lowerindex;
+        while(i<=middle && j<=higherindex)
+        {
+            if(tempMergeArr[i] <= tempMergeArr[j]){
+                array[k]=tempMergeArr[i];
+                i++;
+            }
+
+            else{
+                array[k]=tempMergeArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i<=middle){
+            array[k]=tempMergeArr[i];
+            k++;
+            i++;
         }
     }
     
